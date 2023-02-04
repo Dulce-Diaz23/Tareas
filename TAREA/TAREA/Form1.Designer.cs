b80bb7f02c0372fb5895cb76b32de2ba4306d5e5
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-02-03 22:18:40 -0600

    Tarea1
    Valores positivos, negativos, pares e impares

---
+++ TAREA/TAREA/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace TAREA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumeroTextBox = new System.Windows.Forms.TextBox();
+            this.EjecutarButton = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numero:";
+            // 
+            // NumeroTextBox
+            // 
+            this.NumeroTextBox.Location = new System.Drawing.Point(109, 53);
+            this.NumeroTextBox.Name = "NumeroTextBox";
+            this.NumeroTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NumeroTextBox.TabIndex = 1;
+            // 
+            // EjecutarButton
+            // 
+            this.EjecutarButton.Location = new System.Drawing.Point(135, 102);
+            this.EjecutarButton.Name = "EjecutarButton";
+            this.EjecutarButton.Size = new System.Drawing.Size(75, 23);
+            this.EjecutarButton.TabIndex = 2;
+            this.EjecutarButton.Text = "Ejecutar";
+            this.EjecutarButton.UseVisualStyleBackColor = true;
+            this.EjecutarButton.Click += new System.EventHandler(this.EjecutarButton_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 331);
+            this.Controls.Add(this.EjecutarButton);
+            this.Controls.Add(this.NumeroTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NumeroTextBox;
+        private System.Windows.Forms.Button EjecutarButton;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ TAREA/TAREA/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAREA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EjecutarButton_Click(object sender, EventArgs e)
+        {
+            String numero, nume;
+
+            if (NumeroTextBox.Text == "")
+            {
+                errorProvider.SetError(NumeroTextBox, "Ingrese un valor");
+                return;
+            }
+            errorProvider.Clear();
+
+            int num = Convert.ToInt32(NumeroTextBox.Text);
+            numero = ParImpar(num);
+            nume = PositivoNegativo(num);
+            MessageBox.Show("El valor ingresado es " + numero + " y " + nume, "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //label2.Text = Convert.ToString("El valor ingresado es " + numero + " y " + nume);
+        }
+
+        private String ParImpar(int num)
+        {
+            int n = num % 2;
+            String resultado;
+
+            if (n == 0)
+                resultado = "Par";
+            else
+                resultado = "Impar";
+
+            return resultado;
+        }
+
+        private String PositivoNegativo(int numero)
+        {
+            String resultad2;
+
+            if (numero > 0)
+                resultad2 = "Positivo";
+            else
+                resultad2 = "Negativo";
+
+            return resultad2;
+        }
+    }
+}
+    
