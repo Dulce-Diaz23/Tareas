f6447ce1a9fb57b024c0b794598ed19f91cd2a7b
Dulce-Diaz23 <123528632+Dulce-Diaz23@users.noreply.github.com>
2023-02-09 23:10:53 -0600

    Tarea No. 2
    Cálculo de promedio notas con función asíncrona

---
+++ TAREA/TAREA/TareaEjer2.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace TAREA
+{
+    partial class TareaEjer2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Nota1TextBox = new System.Windows.Forms.TextBox();
+            this.Nota2TextBox = new System.Windows.Forms.TextBox();
+            this.Nota3TextBox = new System.Windows.Forms.TextBox();
+            this.Nota4TextBox = new System.Windows.Forms.TextBox();
+            this.CalcularButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NotaFinalTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nota Parcial 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota Parcial 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota Parcial 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota Parcial 4:";
+            // 
+            // Nota1TextBox
+            // 
+            this.Nota1TextBox.Location = new System.Drawing.Point(137, 31);
+            this.Nota1TextBox.Name = "Nota1TextBox";
+            this.Nota1TextBox.Size = new System.Drawing.Size(100, 26);
+            this.Nota1TextBox.TabIndex = 4;
+            // 
+            // Nota2TextBox
+            // 
+            this.Nota2TextBox.Location = new System.Drawing.Point(137, 70);
+            this.Nota2TextBox.Name = "Nota2TextBox";
+            this.Nota2TextBox.Size = new System.Drawing.Size(100, 26);
+            this.Nota2TextBox.TabIndex = 5;
+            // 
+            // Nota3TextBox
+            // 
+            this.Nota3TextBox.Location = new System.Drawing.Point(137, 110);
+            this.Nota3TextBox.Name = "Nota3TextBox";
+            this.Nota3TextBox.Size = new System.Drawing.Size(100, 26);
+            this.Nota3TextBox.TabIndex = 6;
+            // 
+            // Nota4TextBox
+            // 
+            this.Nota4TextBox.Location = new System.Drawing.Point(137, 149);
+            this.Nota4TextBox.Name = "Nota4TextBox";
+            this.Nota4TextBox.Size = new System.Drawing.Size(100, 26);
+            this.Nota4TextBox.TabIndex = 7;
+            // 
+            // CalcularButton
+            // 
+            this.CalcularButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.CalcularButton.Location = new System.Drawing.Point(314, 268);
+            this.CalcularButton.Name = "CalcularButton";
+            this.CalcularButton.Size = new System.Drawing.Size(92, 36);
+            this.CalcularButton.TabIndex = 8;
+            this.CalcularButton.Text = "Calcular";
+            this.CalcularButton.UseVisualStyleBackColor = false;
+            this.CalcularButton.Click += new System.EventHandler(this.CalcularButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(258, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Nota Final:";
+            // 
+            // NotaFinalTextBox
+            // 
+            this.NotaFinalTextBox.Location = new System.Drawing.Point(349, 191);
+            this.NotaFinalTextBox.Name = "NotaFinalTextBox";
+            this.NotaFinalTextBox.ReadOnly = true;
+            this.NotaFinalTextBox.Size = new System.Drawing.Size(100, 26);
+            this.NotaFinalTextBox.TabIndex = 10;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(469, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 20);
+            this.label6.TabIndex = 11;
+            // 
+            // TareaEjer2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 354);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.NotaFinalTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CalcularButton);
+            this.Controls.Add(this.Nota4TextBox);
+            this.Controls.Add(this.Nota3TextBox);
+            this.Controls.Add(this.Nota2TextBox);
+            this.Controls.Add(this.Nota1TextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "TareaEjer2";
+            this.Text = "TareaEjer2";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Nota1TextBox;
+        private System.Windows.Forms.TextBox Nota2TextBox;
+        private System.Windows.Forms.TextBox Nota3TextBox;
+        private System.Windows.Forms.TextBox Nota4TextBox;
+        private System.Windows.Forms.Button CalcularButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NotaFinalTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ TAREA/TAREA/TareaEjer2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TAREA
+{
+    public partial class TareaEjer2 : Form
+    {
+        public TareaEjer2()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void CalcularButton_Click(object sender, EventArgs e)
+        {
+            if (Nota1TextBox.Text == "" )
+            {
+                errorProvider.SetError(Nota1TextBox, "Ingrese un valor");
+                return;
+            }
+            if ( Nota2TextBox.Text == "" )
+            {
+                errorProvider.SetError(Nota2TextBox, "Ingrese un valor");
+                return;
+            }
+            if ( Nota3TextBox.Text == "" )
+            {
+                errorProvider.SetError(Nota3TextBox, "Ingrese un valor");
+                return;
+            }
+            if ( Nota4TextBox.Text == "" )
+            {
+                errorProvider.SetError(Nota4TextBox, "Ingrese un valor");
+                return;
+            }
+
+            errorProvider.Clear();
+
+
+          decimal nota1 =Convert.ToDecimal(Nota1TextBox.Text);
+          decimal nota2 = Convert.ToDecimal(Nota2TextBox.Text);
+          decimal nota3 = Convert.ToDecimal(Nota3TextBox.Text);
+          decimal nota4 = Convert.ToDecimal(Nota4TextBox.Text);
+
+          decimal notaPromedio = await CalcularPromedioAsync(nota1, nota2, nota3, nota4);
+
+            if (notaPromedio < 65)
+            {
+                label6.Text = "Repobado";
+            }
+            else
+            {
+                label6.Text = "Aprobado";
+            }
+
+          NotaFinalTextBox.Text = notaPromedio.ToString();
+
+           
+          
+        }
+
+        private async Task<decimal> CalcularPromedioAsync(decimal not1, decimal not2, decimal not3, decimal not4)
+        {
+            decimal promedio = await Task.Run(() =>
+            {
+                decimal suma = not1 + not2 + not3 + not4;
+                promedio = suma / 4;
+                return promedio;
+            });
+
+            return promedio;
+        }
+    }
+}
